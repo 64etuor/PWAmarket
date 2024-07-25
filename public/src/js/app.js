d7a7a4799d22090f74d8a1c557da29a7a843e429
64etuor <64etuor@gmail.com>
2024-07-25 23:32:23 +0900

    first commit

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,22 @@
 // public-src-js-app.js
+
+var deferredPrompt;
+
+if (!window.Promise) {
+    window.Promise = promise;
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+        console.log('service worker registered!');
+
+    });
+    window.addEventListener('beforeinstallprompt', function(event) {
+        console.log('beforeinstallprompt fired');
+        event.preventDefault();
+        deferredPrompt = event;
+        return false;
+    })
+}
